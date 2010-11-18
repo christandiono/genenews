9d50590cb02b0030ef7373c41877f2b3bd85d097
Chris Tandiono <chris.tandiono@gmail.com>
2010-11-17 21:58:56 -0800

    Created WSGI script.

--- genenewswsgi.py
+++ genenewswsgi.py
@@ -1,2 +1,10 @@
-sys.path.append('/Users/chris/genenews')
+import sys
 
+sys.path.append('/usr/local/var/genenews')
+sys.path.append('/usr/local/var')
+
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'genenews.settings'
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
