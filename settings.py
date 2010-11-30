e9fbe832d54e82811339423c677796d9012794a3
Chris Tandiono <chris.tandiono@gmail.com>
2010-11-29 23:28:20 -0800

    Updated settings for new hosting

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = '/Users/chris/genenews/db.db'             # Or path to database file if using sqlite3.
+DATABASE_NAME = '/home/chris/genenews/db.db'             # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
@@ -80,7 +80,7 @@ TEMPLATE_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(dirname, 'templates'),
-    '/Library/Python/2.5/site-packages/django/contrib/auth/tests/templates/',
+    '/usr/lib/python2.7/site-packages/django/contrib/auth/tests/templates/',
 )
 
 INSTALLED_APPS = (
--- settings_local.py
+++ settings_local.py
@@ -1,4 +1,4 @@
 DEFAULT_CONTENT_TYPE = 'application/xhtml+xml'
 
-MEDIA_ROOT = '/Users/chris/genenews/media'
+MEDIA_ROOT = '/home/chris/genenews/media'
 MEDIA_URL = '/media/'
